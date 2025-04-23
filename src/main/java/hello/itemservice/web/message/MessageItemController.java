1bc85319db036b2d1278907e268c4a357682d3e8
nameismini <nameismini@gmail.com>
2025-04-24 08:33:50 +0900

    messgae last

--- src/main/java/hello/itemservice/web/message/MessageItemController.java
+++ src/main/java/hello/itemservice/web/message/MessageItemController.java
@@ -25,7 +25,7 @@ public class MessageItemController {
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model) {
+    public String item(@PathVariable("itemId") long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "message/item";
@@ -46,14 +46,14 @@ public class MessageItemController {
     }
 
     @GetMapping("/{itemId}/edit")
-    public String editForm(@PathVariable Long itemId, Model model) {
+    public String editForm(@PathVariable("itemId") Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "message/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable("itemId") Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/message/items/{itemId}";
     }
